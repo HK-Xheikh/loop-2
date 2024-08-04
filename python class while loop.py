1d68f6534f20c86093fe892311f3dd171be38ac3
HK-Xheikh <hariskhan1151@gmail.com>
2024-08-04 15:49:16 +0500

    Add files via upload

---
+++ python class while loop.py
@@ -0,0 +1,36 @@
+i=0
+while i<=10:
+    print("samj aya")
+    i +=1    
+print("reset of the code")
+
+
+
+i=0
+while i<=5:
+    print("huzzi")
+    i +=1  
+    if i==3:  
+        break
+print("reset of the code")
+
+
+i=0
+while i<=5:  
+    if i==3:  
+        i+=1
+        continue
+    print(i)
+    i+=1
+print("reset of the code")
+
+
+num=int(input("enter the number: "))
+sum=0
+i=0
+while i<=num:
+    sum=sum*i
+    i +=1
+print(sum)
+
+
